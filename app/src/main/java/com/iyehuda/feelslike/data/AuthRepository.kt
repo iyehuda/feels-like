5155ed7a65e99f4b0a683e3fad3a714864d04d88
Yehuda Chikvashvili <yehudaac1@gmail.com>
2025-02-27 13:49:40 +0200

    (signup) Add splash screen
    * Added splash screen for startup. This screen now navigates to login or home screen, depends on whether the user is signed in

--- app/src/main/java/com/iyehuda/feelslike/data/AuthRepository.kt
+++ app/src/main/java/com/iyehuda/feelslike/data/AuthRepository.kt
@@ -2,16 +2,14 @@ package com.iyehuda.feelslike.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.iyehuda.feelslike.data.model.UserDetails
 import com.iyehuda.feelslike.data.utils.Result
 
 class AuthRepository(val dataSource: AuthDataSource) {
     private val _userLogin = MutableLiveData<Result<UserDetails>?>().apply {
-        Firebase.auth.currentUser?.let {
-            value = Result.Success(UserDetails.fromUser(it))
-        }
+        value = Firebase.auth.currentUser?.let { Result.Success(UserDetails.fromUser(it)) }
     }
     val userLogin: LiveData<Result<UserDetails>?> = _userLogin
 
