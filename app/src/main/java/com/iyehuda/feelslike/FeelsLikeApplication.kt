5e4a4b559943be0aa9d5408c2605d1bdb10bc6d4
Yehuda Chikvashvili <yehudaac1@gmail.com>
2025-03-01 01:08:43 +0200

    (hilt) Add Hilt dependency injection

---
+++ app/src/main/java/com/iyehuda/feelslike/FeelsLikeApplication.kt
@@ -0,0 +1,7 @@
+package com.iyehuda.feelslike
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FeelsLikeApplication : Application()
--- app/src/main/java/com/iyehuda/feelslike/ui/home/HomeFragment.kt
+++ app/src/main/java/com/iyehuda/feelslike/ui/home/HomeFragment.kt
@@ -6,23 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.iyehuda.feelslike.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View {
-        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
