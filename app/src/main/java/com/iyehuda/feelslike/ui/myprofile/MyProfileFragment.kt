18f24c3016ca1dfac7a3e37d5aa992133e6baf3f
Or Bachar <or.bachar@hunters.ai>
2025-03-17 21:21:38 +0200

    (my profile) Display user posts in My Profile screen
    *   Added a RecyclerView to the My Profile screen to display the user's posts.
    *   Created a PostAdapter to handle the display of post data in the RecyclerView.
    *   Implemented logic in MyProfileViewModel to fetch the user's posts from Firestore and update the RecyclerView.
    * The posts are ordered by date of creation in a descending order.
    * Updated the layout to contain the posts RecyclerView.
    * Added the `postAdapter` variable to the MyProfileFragment class.
    * Removed edit button icon.

--- app/src/main/java/com/iyehuda/feelslike/ui/myprofile/MyProfileFragment.kt
+++ app/src/main/java/com/iyehuda/feelslike/ui/myprofile/MyProfileFragment.kt
@@ -6,16 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.iyehuda.feelslike.R
 import com.iyehuda.feelslike.data.model.UserDetails
 import com.iyehuda.feelslike.databinding.FragmentMyProfileBinding
 import com.iyehuda.feelslike.ui.base.BaseFragment
+import com.iyehuda.feelslike.ui.home.PostAdapter
 import com.iyehuda.feelslike.ui.utils.ImageUtil
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyProfileFragment : BaseFragment<FragmentMyProfileBinding>() {
     private val viewModel: MyProfileViewModel by viewModels()
+    private lateinit var postAdapter: PostAdapter
 
     override fun createBinding(
         inflater: LayoutInflater,
@@ -25,10 +29,16 @@ class MyProfileFragment : BaseFragment<FragmentMyProfileBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupRecyclerView()
+        
         viewModel.userDetails.observe(viewLifecycleOwner) { user ->
             user?.let {
                 updateUserView(it)
             }
+        }
+
+        viewModel.userPosts.observe(viewLifecycleOwner) { posts ->
+            postAdapter.submitList(posts)
         }
 
         binding.editButton.setOnClickListener {
@@ -38,6 +48,14 @@ class MyProfileFragment : BaseFragment<FragmentMyProfileBinding>() {
         binding.logoutButton.setOnClickListener {
             viewModel.logout()
             findNavController().navigate(R.id.action_logout)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        postAdapter = PostAdapter()
+        binding.recyclerViewPosts.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = postAdapter
         }
     }
 
